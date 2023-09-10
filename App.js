70b9fed83d9aba24cbb93efddf385c6a28b37201
Andrew7Silva <reginaldoandrew07@gmail.com>
2023-09-09 23:34:29 -0300

    Modificações na rolagem(Flatlist)
    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredTextMeta, setEnteredTextMeta] = useState("");
+  const [metasList, setMetasList] = useState([]);
+  function metaEntradaHandler(enteredText) {
+    setEnteredTextMeta(enteredText);
+  }
+
+  function metaAddHandler() {
+    setMetasList((atualMetasList) => [
+      ...atualMetasList,
+      { text: enteredTextMeta, key: Math.random().toString() },
+    ]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Metas do seu curso!"
+          onChangeText={metaEntradaHandler}
+        />
+        <Button title="Add Meta" onPress={metaAddHandler} />
+      </View>
+      <View style={styles.metasContainer}>
+        <FlatList
+          data={metasList}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.metaItem}>
+                <Text style={styles.metaText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceHorizontal={false}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  metasContainer: {
+    flex: 5,
+  },
+  metaItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  metaText: {
+    color: "white",
   },
 });
